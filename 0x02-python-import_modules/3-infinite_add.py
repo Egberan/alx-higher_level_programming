de873d9ae99dfbd0043bfba16bf36649c8c382cc
Egberan <aiyidumoses@gmail.com>
2022-09-24 14:00:21 -0700

    commiting

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ != "__main__":
-    exit()
-
-i = 0
-result = 0
-for argument in sys.argv:
-    if i != 0:
-        result += int(argument)
-    else:
-        i += 1
-print("{:d}".format(result))
+if __name__ == '__main__':
+    args = sys.argv
+    args.pop(0)
+    print(sum(map(int, args)))
